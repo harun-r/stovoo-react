2da5d1e0231210f4ddca1f0200acd85e450cd20c
Harun <mdharunbd88@gmail.com>
2022-04-01 14:04:59 +0600

    Faq

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
 const Questions = () => {
     return (
         <div className="question-area">
@@ -8,7 +16,56 @@ const Questions = () => {
                     <h3>Frequently asked questions</h3>
                 </div>
                 <div className="question-ans">
-
+                    <Accordion>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    What is StoVoo
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    StoVoo is a food service & delivery app bringing people and food together in an engaging, fun & exciting way. With features & earning opportunities, you would find anywhere else.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    What is Quicksales
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Quicksales is a 24-hour story feature that allows vendors to market perishable goods more engagingly, allowing users to order right from the story.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    What Makes StoVoo Unique?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    We are not like any other food delivery or food service app; we pride ourselves as the food delivery space disruptor, from swift delivery to inclusiveness to additional earning streams & many more.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    Payment Methods
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    We believe the world is evolving hence stovoo allows our users the flexibility to pay as they wish, from credit cards to debit cards, cryptocurrencies & more.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                    </Accordion>
                 </div>
             </div>
 
