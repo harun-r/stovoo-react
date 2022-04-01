b334886e0fcd1a07a63d344730de8de2cdc39787
Harun <mdharunbd88@gmail.com>
2022-04-01 11:40:15 +0600

    Counter Up

--- src/components/FunFact/FunFact.js
+++ src/components/FunFact/FunFact.js
@@ -1,22 +1,60 @@
 import React from 'react';
-
+import CountUp from 'react-countup';
+import ReactVisibilitySensor from "react-visibility-sensor";
 const FunFact = () => {
     return (
         <div className="fun-fact">
             <div className="container">
                 <div className="fun-fact-inner">
                     <div className="item">
-                        <h3>2M+</h3>
+                        <h3>
+
+                            <CountUp
+                                start={0}
+                                end={10}
+                            >
+                                {({ countUpRef, start }) => (
+                                    <ReactVisibilitySensor onChange={start}>
+                                        <span ref={countUpRef} ></span>
+                                    </ReactVisibilitySensor>
+
+                                )}
+                            </CountUp>
+                            M+</h3>
                         <p>Expected downloads & installations</p>
                     </div>
                     <div className="item">
                         <h3>
-                           345K+
+                            <CountUp
+                                start={0}
+                                end={345}
+                            >
+                                {({ countUpRef, start }) => (
+                                    <ReactVisibilitySensor onChange={start}>
+                                        <span ref={countUpRef} ></span>
+                                    </ReactVisibilitySensor>
+
+                                )}
+                            </CountUp>
+                            K+
                         </h3>
                         <p>Expected daily active transactions</p>
                     </div>
                     <div className="item">
-                        <h3>34K+</h3>
+                        <h3>
+
+                            <CountUp
+                                start={0}
+                                end={34}
+                            >
+                                {({ countUpRef, start }) => (
+                                    <ReactVisibilitySensor onChange={start}>
+                                        <span ref={countUpRef} ></span>
+                                    </ReactVisibilitySensor>
+
+                                )}
+                            </CountUp>
+                            K+</h3>
                         <p>Expected happy vendors worldwide</p>
                     </div>
                 </div>
